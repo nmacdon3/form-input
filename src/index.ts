d1422a9526f2ae04b8963602de7117e6ea9c5a56
Nathan MacDonald <natemacd97@gmail.com>
2022-11-11 12:42:58 -0400

    tailwind installed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Button from "./components/Button";
+import "index.css";
 
 export { Button };
 
 //Can probably remove CommonJS
+//was able to fix build error by running export NODE_OPTIONS=--openssl-legacy-provider
